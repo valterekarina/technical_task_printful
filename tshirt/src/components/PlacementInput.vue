c25dd44f542a1c2d2cdf85f15db12c16b50c8b48
valterekarina <101064793+valterekarina@users.noreply.github.com>
2023-08-23 22:52:34 +0300

    Create PlacementInput.vue

---
+++ tshirt/src/components/PlacementInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h3>{{ placement.name }} Placement</h3>
+      <label>Width: <input v-model="placement.width" /></label>
+      <label>Height: <input v-model="placement.height" /></label>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['placement']
+  };
+  </script>
