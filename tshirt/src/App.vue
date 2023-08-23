f51bd32801893f7d98501a051306ae694f6c824b
valterekarina <101064793+valterekarina@users.noreply.github.com>
2023-08-23 22:54:13 +0300

    Update App.vue

--- tshirt/src/App.vue
+++ tshirt/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -25,4 +25,49 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
+
+<template>
+  <div>
+    <h1>T-Shirt Design App</h1>
+    <placement-input
+      v-for="(placement, index) in placements"
+      :key="placement.name"
+      :placement="placement"
+    />
+    <layer-input
+      v-for="(layer, index) in layers"
+      :key="index"
+      :layer="layer"
+      :layer-index="index"
+    />
+    <placement-canvas
+      v-for="(placement, index) in placements"
+      :key="placement.name"
+      :placement="placement"
+      :layers="layers"
+    />
+  </div>
+</template>
+
+<script>
+import PlacementInput from './components/PlacementInput.vue';
+import LayerInput from './components/LayerInput.vue';
+import PlacementCanvas from './components/PlacementCanvas.vue';
+
+export default {
+  components: {
+    PlacementInput,
+    LayerInput,
+    PlacementCanvas
+  },
+  computed: {
+    placements() {
+      return this.$store.getters.placements;
+    },
+    layers() {
+      return this.$store.getters.layers;
+    }
+  }
+};
+</script>
