916dc367da50f0c648275b589868eaf534a10e65
valterekarina <101064793+valterekarina@users.noreply.github.com>
2023-08-23 22:30:28 +0300

    Update main.js

--- tshirt/src/main.js
+++ tshirt/src/main.js
@@ -1,7 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Vuex from 'vuex'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import Vuex from 'vuex'
 
-Vuex.use(Vuex)
+// Vuex.use(Vuex)
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app');
